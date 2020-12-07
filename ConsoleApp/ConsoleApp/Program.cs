e4964fb2a49f2a3ace5c65cfa919994da1aeed55
marcolop3s <75574725+marcolop3s@users.noreply.github.com>
2020-12-07 13:42:25 +0100

    I like capitals

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's learn git");
+            Console.WriteLine("Let's learn GIT");
         }
     }
 }
